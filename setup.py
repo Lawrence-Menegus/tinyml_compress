3a2a660573c98e5b6204ed102601dadf69b48b86
Larry Menegus <lmenegus7@gmail.com>
2025-03-31 14:31:35 -0400

    update 3/31

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tinyml_compress",
-    version="1.0.1",
+    version="1.0.2",
     description="A library for converting, optimizing, and saving/loading machine learning models.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
